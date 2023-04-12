64480060606cf090a3a0265d06a80d9cd2c3f7d7
Tae-Hyoung Choi <choi.taehyoung@gmail.com>
2023-04-12 19:56:50 +0900

    Add multi-processing feature. Add kiwi Korean-morph (--lcodes ko-kiwi)

--- build_corpus.py
+++ build_corpus.py
@@ -10,7 +10,7 @@ import subprocess
 import math
 import time
 import traceback
-from multiprocessing import Pool
+from multiprocessing import Pool, Queue
 
 logger = logging.getLogger('Build_corpus')
 logger.setLevel(logging.DEBUG)
@@ -18,13 +18,14 @@ logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('[%(asctime)s][%(name)s][%(levelname)s] %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 info    = logger.info
 warning = logger.warning
 debug   = logger.debug
+error   = logger.error
 
 # arguments setting 
 parser = argparse.ArgumentParser()
@@ -38,20 +39,29 @@ lcode = args.lcode
 date  = args.date
 nproc = args.nproc
 
-if lcode == 'ko':
+if lcode == 'ko-kkma':
     from konlpy.tag import Kkma # pip install konlpy. See http://konlpy.org/en/v0.4.4/ for further information.
-    kkma = Kkma()
+    komorph = Kkma()
     info("kkma succesfuly loaded!")
+
+elif lcode == 'ko-kiwi':
+    from kiwipiepy import Kiwi  # pip install kiwipiepy. See https://github.com/bab2min/kiwipiepy for further information.
+    komorph = Kiwi()
+    info("kiwi succesfuly loaded!")
+
 elif lcode == 'ja':
     import MeCab # See https://pypi.python.org/pypi/mecab-python/0.996
     mecab = MeCab.Tagger("-Owakati")
     info("mecab succesfuly loaded!")
+
 elif lcode == 'zh':
     import jieba # See https://pypi.python.org/pypi/jieba/
     info("jieba succesfuly loaded!")
+
 elif lcode == 'vi':
     from pyvi.pyvi import ViTokenizer # See https://pypi.python.org/pypi/pyvi
     info("pyvi succesfuly loaded!")
+
 elif lcode == 'th':  
     import pythai # See https://pypi.python.org/pypi/pythai  
     info("pythai succesfuly loaded!")
@@ -66,7 +76,10 @@ elif lcode == 'th':
 #     print "StanfordSegmenter succesfuly loaded!"
     
 max_corpus_size = args.max_corpus_size
-fname = f'{lcode}wiki-{date}-pages-articles-multistream.xml'
+if lcode in ['ko', 'ko-kkma', 'ko-kiwi']:  # korean
+    fname = f'kowiki-{date}-pages-articles-multistream.xml'
+else:
+    fname = f'{lcode}wiki-{date}-pages-articles-multistream.xml'
 
 def clean_text(text):
     global lcode
@@ -84,7 +97,7 @@ def clean_text(text):
     text = regex.sub("[']{3}", "", text) # remove bold symbols
     text = regex.sub("[']{2}", "", text) # remove italic symbols
     
-    if lcode in ['ko']: # korean
+    if lcode in ['ko', 'ko-kkma', 'ko-kiwi']: # korean
         text = regex.sub(u"[^ \r\n\p{Hangul}.?!]", " ", text) # Replace unacceptable characters with a space.
     elif lcode in ['ja']: # japanese
         text = regex.sub(u"[^\r\n\p{Han}\p{Hiragana}\p{Katakana}ー。！？]", "", text)
@@ -129,8 +142,10 @@ def sentence_segment(text):
     elif lcode in ['de']: # german
         sents = regex.split("([.?!])?[\n]+|[.?!] ", text)
         sents = [sent[0].lower() + sent[1:] for sent in sents if sent is not None and len(sent) > 1]
-    else:
+    elif lcode in ['ko', 'ko-kkma']:
         sents = regex.split("([.?!])?[\n]+|[.?!] ", text)
+    elif lcode in ['ko-kiwi']:
+        sents = komorph.split_into_sents(text)
     return sents
         
 def word_segment(sent):
@@ -142,8 +157,10 @@ def word_segment(sent):
       A list of words.
     '''
     global lcode
-    if lcode in ['ko']:
-        words = [word for word, _ in kkma.pos(sent)]
+    if lcode in ['ko-kkma']:
+        words = [word for word, _ in komorph.pos(sent)]
+    elif lcode in ['ko-kiwi']:
+        words = [token.form for token in komorph.tokenize(sent.text)]
     elif lcode in ['ja']:
         words = mecab.parse(sent.encode('utf8')).split() 
     elif lcode in ['th']:
@@ -158,7 +175,6 @@ def word_segment(sent):
         words = sent.split()
     
     return words
-
 
 def count_size(file):
     command = f"grep -o '<text ' {file} | wc -l"
@@ -184,63 +200,37 @@ def process_text(text):
     return output
 
 
-def mprocess_text(text):
-    text   = clean_text(text)
-    sents  = sentence_segment(text)
-    output = ''
-
-    for sent in sents:
-        if sent is not None and sent != '' and sent != ' ':
-            line = ''
-            words = word_segment(sent)
-            if len(words) > 10:
-                if lcode in ['ja']:
-                    line = " ".join(words).decode('utf8') + "\n"
-                else:
-                    line = " ".join(words) + "\n"
-                output += line
-    return output
-
-"""
-def mprocess_text(pid, q_in, q_out, i, size):
-    while True:
-        if q_in.empty():
-            debug(f'[{pid}] : queue is empty. wait for queueing...')
-            time.sleep(0.1)
-        else:
-            idx, text = q_in.get()
-
-            if idx == -1 and text == '# EXIT #':
-                break
-            else:
-                info(f'[{pid}] start process_text.')
-                try:
-                    output = process_text(text)
-                except Exception as e:
-                    debug(f'[{pid}] Exception occured : {e}')
-                    traceback.print_exc()
-                    continue
-                ci = i.value
-                i.value += 1
-                progress = round(ci/size*100, 2)
-                info(f'[{pid}] [{ci}/{size}]: {progress}% elapsed')
-                q_out.append(output)
-"""
+elem_size = 0
+job_no = 0
+prev_rate = -1
+results = Queue()
 
 
-def callback_func(result):
-    text = result.strip()
-    if len(text) > 10:
-        info(f'{os.getpid()} : {text[:10]}...')
+def progress(result):
+    global elem_size, job_no, prev_rate, results
+    if type(result) is str:
+        results.put(result)
     else:
-        info(f'{os.getpid()} : {text}')
+        debug(f'result : {type(result)}, {result}')
+    job_no +=1
+    i = job_no
+    rate = round(i / elem_size * 100, 1)
+    if prev_rate != rate:
+        if rate.is_integer():
+            info(f'[{i}/{elem_size}]: {int(rate)} %')
+        else:
+            info(f'[{i}/{elem_size}]: {rate} %')
+        prev_rate = rate
+
 
 def build_corpus():
-    global lcode, max_corpus_size, fname
+    global lcode, max_corpus_size, fname, elem_size
     target_file = f"data/{fname}"
 
-    if lcode in ['ko', 'ja', 'th', 'vi', 'zh']:
+    if lcode in ['ja', 'th', 'vi', 'zh']:
         output_file = f"data/{lcode}.txt"
+    elif lcode in ['ko', 'ko-kkma', 'ko-kiwi']:
+        output_file = "data/ko.txt"
     else:
         info(f'Improper target language code : {lcode}')
         exit(0)
@@ -248,14 +238,15 @@ def build_corpus():
     output = ''
     i = 0
     ns = "{http://www.mediawiki.org/xml/export-0.10/}" # namespace
+
     info(f'Start loading file : {target_file}')
     elem_iter = ET.iterparse(target_file, tag=ns+"text")
     info(f'Loaded file : {target_file}')
 
+    elem_size = count_size(target_file)
+    info(f'Total {elem_size} elements in the target file : {target_file}')
+
     if nproc == 1:
-        info('count total size of elements')
-        elem_size = count_size(target_file)
-        info(f'Total {elem_size} elements in the target file : {target_file}')
         for _, elem in elem_iter:
             i += 1
 
@@ -267,42 +258,36 @@ def build_corpus():
                 continue # it's okay as we have a pretty big corpus!
             elem.clear() # We need to save memory!
 
-            progress  = round(i/elem_size*100, 2)
-            debug(f'[{i}/{elem_size}]: {progress}% elapsed')
+            rate = round(i/elem_size*100, 1)
+            debug(f'[{i}/{elem_size}]: {rate}% elapsed')
 
             if len(output) > max_corpus_size:
                 break
 
+        with codecs.open(output_file, 'w', 'utf-8') as fout:
+            fout.write(output)
+
     elif nproc > 1:
-        size = 0
         p = Pool(nproc)
-        results = []
         for _, elem in elem_iter:
-            size += 1
             try:
-                fout = p.apply_async(process_text, (elem.text,), callback=callback_func)
+                fout = p.apply_async(process_text, (elem.text,), callback=progress)
             except Exception as e:
-                debug(f'Exception occured : {e}')
+                error(f'Exception occured : {e}')
                 traceback.print_exc()
                 elem.clear()
                 continue # it's okay as we have a pretty big corpus!
             elem.clear() # We need to save memory!
 
-            results.append(fout)
-
         p.close()
         p.join()
 
-        i = 0
-        for r in results:
-            i += 1
-            line = r.get()
-            output += line
-            progress  = round(i/size*100, 2)
-            debug(f'[{i}/{size}]: {progress}% elapsed')
-
-    with codecs.open(output_file, 'w', 'utf-8') as fout:
-        fout.write(output)
+        with codecs.open(output_file, 'w', 'utf-8') as fout:
+            while True:
+                line = results.get()
+                fout.write(line)
+                if results.empty():
+                    break
 
 
 if __name__ == "__main__":
